e3a026912fccb62b833ef0b250138961595de97a
RanjanDemo <ranjanmahato792@gmail.com>
2026-06-15 19:32:25 +0545

    Task 4 added

---
+++ Day1/Task4/check.py
@@ -0,0 +1,5 @@
+num = 45
+is_between = num > 10 and num < 100
+
+print(f"Number: {num}")
+print(f"Is number greater than 10 and less than 100? {is_between}")
---
+++ Day1/Task4/comparision.py
@@ -0,0 +1,8 @@
+a= int(input("Enter first number: "))
+b= int(input("Enter second number: "))
+if a > b:
+    print(f"{a} is greater than {b}")
+elif a < b:
+    print(f"{a} is less than {b}")
+else:
+    print(f"{a} is equal to {b}")
