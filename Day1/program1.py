e4d91c8c278d906202863b1b1a5113d1e68bc8e7
RanjanDemo <ranjanmahato792@gmail.com>
2026-06-15 19:23:15 +0545

    Task1 all program completed

--- Day1/program1.py
+++
@@ -1,6 +0,0 @@
-name= "Ranjan Kumar Mahato"
-age= 20
-city= "Janakpur, Nepal"
-print("Name:", name)
-print("Age:", age)
-print("City:", city)
--- Day1/program2.py
+++
@@ -1,8 +0,0 @@
-x=20
-y=30.6
-z="Ranjan"
-p=True
-print(type(x))
-print(type(y))
-print(type(z))
-print(type(p))
